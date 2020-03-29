ce58da5cd7f386e6dd6d08c41129822c0ec91ced
Andrei <38653807+AndreiKononov@users.noreply.github.com>
2020-03-29 10:02:31 +0300

    Separated template/styles from component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'my-app',
-    template: `<h1>Hello Angular 9</h1>
-               <p>Angular 9 provides a modular architecture of the application</p>`,
-    styles: [` 
-            h1, h2{color:navy;}
-            p{font-size:13px;}
-            :host { 
-              font-family: Verdana;
-              color: #555;
-            } 
-    `] // styles applies to current component only. Selector "host" is linked to element in which component is hosted (element <my-app>, in this case)
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'] // styles applies to current component only. Selector "host" is linked to element in which component is hosted (element <my-app>, in this case)
 })
 
 export class AppComponent { }
