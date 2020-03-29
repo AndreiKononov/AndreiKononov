74c706957846b63a109b77cd5811c0e8d6866229
Andrei <38653807+AndreiKononov@users.noreply.github.com>
2020-03-29 09:50:21 +0300

    Added styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,16 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'my-app',
-    template: `<label>Insert your name:   </label>
-                  <input [(ngModel)]="name" placeholder="name">
-                  <h1>Welcome {{name}}!</h1>`
+    template: `<h1>Hello Angular 9</h1>
+               <p>Angular 9 provides a modular architecture of the application</p>`,
+    styles: [` 
+            h1, h2{color:navy;}
+            p{font-size:13px;}
+            :host { 
+              font-family: Verdana;
+              color: #555;
+            } 
+    `] // styles applies to current component only. Selector "host" is linked to element in which component is hosted (element <my-app>, in this case)
 })
 
-export class AppComponent {
-    name ='';
-}
+export class AppComponent { }
