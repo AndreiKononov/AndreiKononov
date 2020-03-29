9fb93a0c5004fd033c888ce18eb51aa5170087ef
Andrei <38653807+AndreiKononov@users.noreply.github.com>
2020-03-30 00:04:57 +0300

    Added a few comments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 
 @NgModule({
-    imports:        [ BrowserModule, FormsModule],
-    declarations:   [ AppComponent ],
-    bootstrap:      [ AppComponent ],
+    imports:        [ BrowserModule, FormsModule], // set of another modules are used in current module
+    declarations:   [ AppComponent ], // set of view classes of current module (There are three type of view classes in Angular: components, directives, pipes)
+    bootstrap:      [ AppComponent ], //root component (runs by default when this app starts running)
+    exports:        [], // set of view classes which will be use
+    providers:      [] // set of classes which create services for current module
 })
 
 export class AppModule { }
