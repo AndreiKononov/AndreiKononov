c77d5aec298157348e05305be9f1a4c76b386aae
Andrei <38653807+AndreiKononov@users.noreply.github.com>
2020-03-29 14:38:49 +0300

    Using of a few css files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'my-app',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'] // styles applies to current component only. Selector "host" is linked to element in which component is hosted (element <my-app>, in this case)
+    styleUrls: [
+        './app.component.css',
+        './app.component2.css',
+    ] // styles applies to current component only. Selector "host" is linked to element in which component is hosted (element <my-app>, in this case)
 })
 
 export class AppComponent { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,10 @@ import { AppComponent } from "./app.component";
 
 @NgModule({
     imports:        [ BrowserModule, FormsModule], // set of another modules are used in current module
-    declarations:   [ AppComponent ], // set of view classes of current module (There are three type of view classes // in Angular: components, directives, pipes)
+    declarations:   [ AppComponent ], // set of view classes of current module (There are three type of view classes in Angular: components, directives, pipes)
     bootstrap:      [ AppComponent ], //root component (runs by default when this app starts running)
-    exports: [], // set of view classes which will be use
-    providers: [] // set of classes which create services for current module
+    exports:        [], // set of view classes which will be use
+    providers:      [] // set of classes which create services for current module
 })
 
 export class AppModule { }
