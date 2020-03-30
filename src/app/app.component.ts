13abf3bf75a4aea2ff2a66880d7c2fbcb59965dc
Andrei <38653807+AndreiKononov@users.noreply.github.com>
2020-03-30 23:19:57 +0300

    #template variable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,21 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'my-app',
-    template: `<label>Insert your name:   </label>
-                  <input [(ngModel)]="name" placeholder="name">
-                  <h1>Welcome {{name}}!</h1>`
+    template: `
+        <p #userName>{{name}}</p> <!-- initialisation of template variable using #. We can use this variable inside template only -->
+        <p>{{userName.textContent}}</p>
+        <input type="text" [(ngModel)]="name" placeholder="name">
+        <hr>
+        <child-comp #counter></child-comp>
+        <button (click)="counter.increment()">+</button> <!-- using in template only -->
+        <button (click)="counter.decrement()">-</button>
+    `
 })
 
 export class AppComponent {
-    name ='';
+    name: string = 'Andrei';
+
+    // increment() { this.counter++; }   variable "counter" doesn't exist in component
+    // decrement() { this.counter--; }  -- // -- // --
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
+import { ChildComponent } from "./child.component";
 
 @NgModule({
     imports:        [ BrowserModule, FormsModule], // set of another modules are used in current module
-    declarations:   [ AppComponent ], // set of view classes of current module (There are three type of view classes in Angular: components, directives, pipes)
+    declarations:   [ AppComponent, ChildComponent ], // set of view classes of current module (There are three type of view classes in Angular: components, directives, pipes)
     bootstrap:      [ AppComponent ], //root component (runs by default when this app starts running)
     exports:        [], // set of view classes which will be use
     providers:      [] // set of classes which create services for current module
